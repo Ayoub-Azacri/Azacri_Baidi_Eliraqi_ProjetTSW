102a807ded1173f0679d1ff3a0eddf1fbb39893c
Ayoub-Azacri <ayoub.azacri@gmail.com>
2020-12-06 17:16:47 +0100

    wipe:work in progress

--- JAVASCRIPT/vanilla-calendar.js
+++ JAVASCRIPT/vanilla-calendar.js
@@ -1,9 +1,3 @@
-/*
-    Vanilla AutoComplete v0.1
-    Copyright (c) 2019 Mauro Marssola
-    GitHub: https://github.com/marssola/vanilla-calendar
-    License: http://www.opensource.org/licenses/mit-license.php
-*/
 let VanillaCalendar = (function () {
     function VanillaCalendar(options) {
         function addEvent(el, type, handler){

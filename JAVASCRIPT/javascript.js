f9be5ba05e81797f7727d757a3f8641d339d43ee
Ayoub-Azacri <ayoub.azacri@gmail.com>
2020-12-07 21:28:29 +0100

    wipe:work in progress

--- JAVASCRIPT/javascript.js
+++ JAVASCRIPT/javascript.js
@@ -10,49 +10,82 @@ let pastDates = true, availableDates = false, availableWeekDays = false
             })
             
             
-            let btnPastDates = document.querySelector('[name=pastDates]')
-            btnPastDates.addEventListener('click', () => {
-                pastDates = !pastDates
-                calendar.set({pastDates: pastDates})
-                btnPastDates.innerText = `${(pastDates ? 'Disable' : 'Enable')} past dates`
-            })
+            // let btnPastDates = document.querySelector('[name=pastDates]')
+            // btnPastDates.addEventListener('click', () => {
+            //     pastDates = !pastDates
+            //     calendar.set({pastDates: pastDates})
+            //     btnPastDates.innerText = `${(pastDates ? 'Disable' : 'Enable')} past dates`
+            // })
             
-            let btnAvailableDates = document.querySelector('[name=availableDates]')
-            btnAvailableDates.addEventListener('click', () => {
-                availableDates = !availableDates
-                btnAvailableDates.innerText = `${(availableDates ? 'Clear available dates' : 'Set available dates')}`
-                if (!availableDates) {
-                    calendar.set({availableDates: [], datesFilter: false})
-                    return
-                }
-                let dates = () => {
-                    let result = []
-                    for (let i = 1; i < 15; ++i) {
-                        if (i % 2) continue
-                        let date = new Date(new Date().getTime() + (60 * 60 * 24 * 1000) * i)
-                        result.push({date: `${String(date.getFullYear())}-${String(date.getMonth() + 1).padStart(2, 0)}-${String(date.getDate()).padStart(2, 0)}`})
-                    }
-                    return result
-                }
-                calendar.set({availableDates: dates(), availableWeekDays: [], datesFilter: true})
-            })
+            // let btnAvailableDates = document.querySelector('[name=availableDates]')
+            // btnAvailableDates.addEventListener('click', () => {
+            //     availableDates = !availableDates
+            //     btnAvailableDates.innerText = `${(availableDates ? 'Clear available dates' : 'Set available dates')}`
+            //     if (!availableDates) {
+            //         calendar.set({availableDates: [], datesFilter: false})
+            //         return
+            //     }
+            //     let dates = () => {
+            //         let result = []
+            //         for (let i = 1; i < 15; ++i) {
+            //             if (i % 2) continue
+            //             let date = new Date(new Date().getTime() + (60 * 60 * 24 * 1000) * i)
+            //             result.push({date: `${String(date.getFullYear())}-${String(date.getMonth() + 1).padStart(2, 0)}-${String(date.getDate()).padStart(2, 0)}`})
+            //         }
+            //         return result
+            //     }
+            //     calendar.set({availableDates: dates(), availableWeekDays: [], datesFilter: true})
+            // })
             
-            let btnAvailableWeekDays = document.querySelector('[name=availableWeekDays]')
-            btnAvailableWeekDays.addEventListener('click', () => {
-                availableWeekDays = !availableWeekDays
-                btnAvailableWeekDays.innerText = `${(availableWeekDays ? 'Clear available weekdays' : 'Set available weekdays')}`
-                if (!availableWeekDays) {
-                    calendar.set({availableWeekDays: [], datesFilter: false})
-                    return
+            // let btnAvailableWeekDays = document.querySelector('[name=availableWeekDays]')
+            // btnAvailableWeekDays.addEventListener('click', () => {
+            //     availableWeekDays = !availableWeekDays
+            //     btnAvailableWeekDays.innerText = `${(availableWeekDays ? 'Clear available weekdays' : 'Set available weekdays')}`
+            //     if (!availableWeekDays) {
+            //         calendar.set({availableWeekDays: [], datesFilter: false})
+            //         return
+            //     }
+            //     let days = [{
+            //         day: 'monday'
+            //     }, {
+            //         day: 'tuesday'
+            //     }, {
+            //         day: 'wednesday'
+            //     }, {
+            //         day: 'friday'
+            //     }]
+            //     calendar.set({availableWeekDays: days, availableDates: [], datesFilter: true})
+            // })
+
+            $(".nest").on("click", function() {
+                if (
+                 $(this)
+                  .next()
+                  .hasClass("show")
+                ) {
+                 $(this)
+                  .next()
+                  .removeClass("show");
+                 $(this)
+                  .next()
+                  .slideUp(300);
+                } else {
+                 $(this)
+                  .parent()
+                  .parent()
+                  .find("li .inner")
+                  .removeClass("show");
+                 $(this)
+                  .parent()
+                  .parent()
+                  .find("li .inner")
+                  .slideUp(300);
+                 $(this)
+                  .next()
+                  .toggleClass("show");
+                 $(this)
+                  .next()
+                  .slideToggle(300);
                 }
-                let days = [{
-                    day: 'monday'
-                }, {
-                    day: 'tuesday'
-                }, {
-                    day: 'wednesday'
-                }, {
-                    day: 'friday'
-                }]
-                calendar.set({availableWeekDays: days, availableDates: [], datesFilter: true})
-            })
+               });
+               
